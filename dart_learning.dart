c87529f19905ba2125a3a917daf242e15a049522
VKovalev <npqHqJKee@gmail.com>
2024-05-08 17:07:22 +0300

    Class?

--- dart_learning.dart
+++ dart_learning.dart
@@ -1,11 +1,35 @@
 void main() {
-  someFunction();
+  Point p1 = Point();
+  Point p2 = Point();
+  Point p3 = Point();
+
+  int a = 1;
+  int b = 5;
+
+  print(a + ++b);
+  print('b: $b');
+  someFunction('Google', 21, name: 'Boolba', age: 25);
 }
 
-bool IsNumberOdd(int number) {
-  return number % 2 == 0;
+class Point {
+  Point? _next = null;
+  Point? _previous = null;
+
+  Point({Point? next = null, Point? previous = null}) {
+    this._next = next;
+    this._previous = previous;
+  }
+
+  void setNextPoint(Point point) {
+    this._next = point;
+  }
+
+  void setPreviousPoint(Point point) {
+    this._previous = point;
+  }
 }
 
-void someFunction() {
-  print(IsNumberOdd(999));
+dynamic someFunction(String some_name, int some_age,
+    {String? name, int age = 0}) {
+  return name ?? 'Alloha';
 }
